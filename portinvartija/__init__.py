59ca66eada646c19c20508862fbd0630a33ae0bf
Stefan Hagen <stefan@hagen.link>
2023-10-29 14:57:22 +0100

    Bumped impl version for release 2023.10.29

--- portinvartija/__init__.py
+++ portinvartija/__init__.py
@@ -1,6 +1,6 @@
 # [[[fill git_describe()]]]
-__version__ = '2022.12.13+parent.ff7049ef'
-# [[[end]]] (checksum: 853a9f21e62834d23aa0601ad62a61ce)
+__version__ = '2023.10.29+parent.e53151b0'
+# [[[end]]] (checksum: 065626152e6d0d8e21024deba1bbba5b)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
