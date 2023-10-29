9423dea044b3a16ab25645248e7a8b76041407d6
Stefan Hagen <stefan@hagen.link>
2023-10-29 14:56:08 +0100

    Happiness through comments ...

--- portinvartija/portinvartija.py
+++ portinvartija/portinvartija.py
@@ -11,8 +11,8 @@ from first import first
 
 def it_starts_with_silence() -> None:
     """Stub to bootstrap development and survive the make rounds before implementation is in place."""
-    assert os.getenv('DO_NOT_COMPLAIN_IF_THIS_IS_PRESENT_IN_YOUR_ENV', 'all good') == 'all good'
-    assert sys.path > []
+    assert os.getenv('DO_NOT_COMPLAIN_IF_THIS_IS_PRESENT_IN_YOUR_ENV', 'all good') == 'all good'  # nosec B101
+    assert sys.path > []  # nosec B101
     doc = kdl.Document()
     doc.append(
         kdl.Node(
@@ -30,8 +30,8 @@ def it_starts_with_silence() -> None:
         )
     )
     ast = kdl.parse(doc)
-    assert next(ast[0]) == 123
-    assert first((1, 2)) == 1
+    assert next(ast[0]) == 123  # nosec B101
+    assert first((1, 2)) == 1  # nosec B101
 
 
 def main(argv: Union[List[str], None] = None) -> int:
